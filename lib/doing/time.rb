47d6661fc53b13c35663dd627c82344ebb2ae09b
Brett Terpstra <me@brettterpstra.com>
2022-01-15 08:25:54 -0600

    Clean up relative dates
    - IMPROVED: When showing relative dates, don't include the year if the
    date is the previous year but a later month than the current month (less
    than a year old)

--- lib/doing/time.rb
+++ lib/doing/time.rb
@@ -8,10 +8,10 @@ module Doing
         strftime('%_I:%M%P')
       elsif self > (Date.today - 6).to_time
         strftime('%a %_I:%M%P')
-      elsif self.year == Date.today.year
+      elsif self.year == Date.today.year || (self.year + 1 == Date.today.year && self.month > Date.today.month)
         strftime('%m/%d %_I:%M%P')
       else
-        strftime('%m/%d/%Y %_I:%M%P')
+        strftime('%m/%d/%y %_I:%M%P')
       end
     end
 
