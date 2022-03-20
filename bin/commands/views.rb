ac4275385fe36cfa87e6941c1299865ee46f7c4c
Brett Terpstra <me@brettterpstra.com>
2022-03-20 06:00:18 -0500

    JSON view editing
    - NEW: Use `doing views [-e] -o json NAME` to dump or edit a view as json instead of YAML

--- bin/commands/views.rb
+++ bin/commands/views.rb
@@ -1,11 +1,19 @@
 # @@views
-desc 'List available custom views. Specify a single view to see its YAML configuration.'
+desc 'List available custom views. Specify view names to see YAML configurations.'
 command :views do |c|
+  c.example 'doing views', desc: 'list all views'
+  c.example 'doing views -c', desc: 'list views in column, ideal for shell completion'
+  c.example 'doing views color', desc: 'dump the YAML for a single view'
+  c.example 'doing views -e color', desc: 'edit the YAML configuration for a single view'
+
   c.desc 'List in single column'
   c.switch %i[c column], default_value: false
 
   c.desc 'Open YAML for view in editor (requires argument)'
   c.switch %i[e editor]
+
+  c.desc 'Output/edit view configs as JSON'
+  c.switch %i[j json]
 
   c.action do |_global_options, options, args|
     if args.count.positive?
@@ -13,10 +21,16 @@ command :views do |c|
       args.each { |v| views[v] = @wwid.get_view(v) }
 
       if options[:editor]
-        res = YAML.safe_load(@wwid.fork_editor(YAML.dump(views), message: nil))
+        res = if options[:json]
+                JSON.parse(@wwid.fork_editor(JSON.pretty_generate(views), message: nil))
+              else
+                YAML.safe_load(@wwid.fork_editor(YAML.dump(views), message: nil))
+              end
         args.each { |v| Doing.set("views.#{v}", res[v]) }
         Doing::Util.write_to_file(Doing.config.config_file, YAML.dump(Doing.settings), backup: true)
         Doing.logger.warn('Config:', "#{Doing.config.config_file} updated")
+      elsif options[:json]
+        print JSON.pretty_generate(views)
       else
         print YAML.dump(views)
       end
