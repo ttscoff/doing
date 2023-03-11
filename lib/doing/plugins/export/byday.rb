aad949f218d691a710d8d94772c96ef8785f440d
Brett Terpstra <me@brettterpstra.com>
2023-03-11 05:50:37 -0600

    Formatting for byday table
    - IMPROVED: Formatting and color for byday table
    - IMPROVED: Remove @done tag from byday items

--- lib/doing/plugins/export/byday.rb
+++ lib/doing/plugins/export/byday.rb
@@ -35,27 +35,29 @@ module Doing
         end
       end
 
-      divider = "+----------+------------------------------------------------------------------------------------------------+--------+"
+      divider = "{wd}+{xk}#{'-' *10}{wd}+{xk}#{'-' * 60}{wd}+{xk}#{'-' * 8}{wd}+{x}"
       out = []
+      out << divider
+      out << '{wd}|{xm}date      {wd}|{xbw}item                                                        {wd}|{xy}duration{wd}|{x}'
       out << divider
       days.each do |day, day_items|
         first = day_items.slice!(0, 1)[0]
         interval = wwid.get_interval(first, formatted: true) || '00:00:00'
-        out << "|#{day}|#{first.title.trunc(94).ljust(96)}|#{interval}|"
+        out << "{wd}|{xm}#{day}{wd}|{xbw}#{first.title.tag('done', remove: true).trunc(58).ljust(60)}{wd}|{xy}#{interval}{wd}|{x}"
         day_items.each do |item|
           interval = wwid.get_interval(item, formatted: true) || '00:00:00'
-          out << "|          |#{item.title.trunc(94).ljust(96)}|#{interval}|"
+          out << "{wd}|          |{xbw}#{item.title.tag('done', remove: true).trunc(58).ljust(60)}{wd}|{xy}#{interval}{wd}|{x}"
         end
         day_total = "Total: #{totals[day].time_string(format: :clock)}"
         out << divider
-        out << "|#{day_total.rjust(116)}|"
+        out << "{wd}|{xg}#{day_total.rjust(80)}{wd}|{x}"
         out << divider
       end
       all_total = "Grand Total: #{total.time_string(format: :clock)}"
       out << divider
-      out << "|#{all_total.rjust(116)}|"
+      out << "{wd}|{xrb}#{all_total.rjust(80)}{wd}|{x}"
       out << divider
-      Doing::Pager.page out.join("\n")
+      Doing::Pager.page Doing::Color.template(out.join("\n"))
     end
 
     Doing::Plugins.register 'byday', :export, self
